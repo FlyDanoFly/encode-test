88ec2bd0461283ba37543d33ba5acc3f70bbdbf3
Dano! <dano@skilljar.com>
2018-03-19 09:18:09 -0700

    Adding in DECREFs, very messy, doing profiling

--- mymodule.c
+++ mymodule.c
@@ -30,7 +30,7 @@ int gangsta_div(uint64_t high, uint64_t low, uint64_t *quot_result, uint64_t *re
   //printf("hi: %llu\n", a_hi);
 
   // denominator
-  uint64_t b = 11;
+  uint64_t b = 36;
 
   // quotient
   uint64_t q = a_lo << 1;
@@ -145,7 +145,7 @@ int trike_div(uint64_t high, uint64_t low, char **pp) {
 
   *pp = p+1;
   //printf("here: %s\n", p+1);
-  assert(strcmp("6h7yzb4z112efh4n44afgst7a", buffer) == 0);
+  assert(strcmp("6h7yzb4z112efh4n44afgst7a", *pp) == 0);
   return 0;
 }
 
@@ -169,7 +169,7 @@ int trike_div2(uint64_t high, uint64_t low, char **pp) {
   uint64_t pos2 = low >> 32;
   uint64_t pos34 = high;
   
-  //  int divisor = 36;
+  const  int divisor = 36;
   uint64_t quot;
   uint64_t mod;
   while (pos34) {
@@ -208,7 +208,7 @@ int trike_div2(uint64_t high, uint64_t low, char **pp) {
 
   *pp = p + 1;
   //  printf("here: %s\n", p+1);
-  assert(strcmp("6h7yzb4z112efh4n44afgst7a", buffer) == 0);
+  assert(strcmp("6h7yzb4z112efh4n44afgst7a", *pp) == 0);
   return 0;
 }
 
@@ -226,6 +226,48 @@ static PyObject* py_myFunction(PyObject* self, PyObject* args)
 {
   char *s = "Hello from C!";
   return Py_BuildValue("s", s);
+}
+
+/*
+ * Function to be called from Python
+ */
+unsigned long glob = 0;
+static PyObject* py_myFunction2(PyObject* self, PyObject* args)
+{
+  char buffer[30];
+  PyObject *obj;
+  PyArg_ParseTuple(args, "O", &obj);
+  PyObject *base36 = PyLong_FromLong(36);
+  Py_INCREF(base36);
+  PyObject *rem = 0;
+  PyObject *div = 0;
+  div = obj;
+  Py_INCREF(div);
+  PyObject *mod = 0;
+  int i;
+  for (i = 0; i < 30; ++i)
+    buffer[i] = 0;
+  char *p = &buffer[30-1];
+
+  int ib = 0;
+  //  while (ib < 5) {
+  while (PyLong_AsSsize_t(div) != 0L) {
+    PyErr_Clear();
+    rem = PyNumber_Divmod(div, base36);      
+    Py_DECREF(div);
+    div = PyTuple_GetItem(rem, 0);
+    Py_INCREF(div);
+    mod = PyTuple_GetItem(rem, 1);
+    *(--p) = trans_table[PyLong_AsLong(mod)];
+    Py_DECREF(rem);
+    ib += 1;
+    glob += 1;
+    //printf("glob %lu\n", glob);
+  }
+  Py_DECREF(div);
+  Py_DECREF(base36);
+  Py_DECREF(base36);
+  return Py_BuildValue("s", p);
 }
 
 /*
@@ -248,28 +290,33 @@ static PyObject* py_myfn0(PyObject* self, PyObject* args)
 }
 #endif 
 
+
 static PyObject* py_myfn(PyObject* self, PyObject* args)
 {
   char buffer[30];
   PyObject *obj;
   PyArg_ParseTuple(args, "O", &obj);
   PyObject *base36 = PyLong_FromLong(36);
-  PyObject *rem;
-  PyObject *div = obj;
-  PyObject *mod;
+  PyObject *rem = 0;
+  PyObject *div = 0;
+  div = obj;
+  Py_INCREF(div);
+  PyObject *mod = 0;
   int i;
   for (i = 0; i < 30; ++i)
     buffer[i] = 0;
   char *p = &buffer[30-1];
+  long long whee_test;
   unsigned long long whee;
   unsigned long long whee_d;
   unsigned long long whee_m;
 
   while (1) {
-    whee = PyLong_AsUnsignedLongLong(div);
-    if (whee != -1) {
+    whee_test = PyLong_AsUnsignedLongLong(div);
+    if (whee_test != -1L) {
       PyErr_Clear();
       // do it the fast way
+      whee = PyLong_AsUnsignedLongLong(div);
       whee_d = whee;
       while (whee_d != 0) {
 	whee = whee_d;
@@ -281,14 +328,17 @@ static PyObject* py_myfn(PyObject* self, PyObject* args)
     }
     else {
       rem = PyNumber_Divmod(div, base36);
+      Py_DECREF(div);
       div = PyTuple_GetItem(rem, 0);
+      Py_INCREF(div);
       mod = PyTuple_GetItem(rem, 1);
       *(--p) = trans_table[PyLong_AsLong(mod)];
+      Py_DECREF(rem);
     }
   }
-  //printf("yo %s\n", p);
-  return PyUnicode_FromString(p);
-  return obj;
+  Py_DECREF(base36);
+  Py_DECREF(div);
+  return Py_BuildValue("s", p);
 }
 
 static PyObject* py_myfn2(PyObject* self, PyObject* args)
@@ -297,9 +347,11 @@ static PyObject* py_myfn2(PyObject* self, PyObject* args)
   PyObject *obj;
   PyArg_ParseTuple(args, "O", &obj);
   PyObject *base36 = PyLong_FromLong(36);
-  PyObject *rem;
-  PyObject *div = obj;
-  PyObject *mod;
+  PyObject *rem = 0;
+  PyObject *div = 0;
+  div = obj;
+  Py_INCREF(div);
+  PyObject *mod = 0;
   int i;
   for (i = 0; i < 30; ++i)
     buffer[i] = 0;
@@ -307,14 +359,16 @@ static PyObject* py_myfn2(PyObject* self, PyObject* args)
   while (PyLong_AsSsize_t(div) != 0L) {
     PyErr_Clear();
     rem = PyNumber_Divmod(div, base36);
+    Py_DECREF(div);
     div = PyTuple_GetItem(rem, 0);
+    Py_INCREF(div);
     mod = PyTuple_GetItem(rem, 1);
     *(--p) = trans_table[PyLong_AsLong(mod)];
-    //    Py_ssize_t v = PyLong_AsSsize_t(div);
+    Py_DECREF(rem);
   }
-  //printf("yo %s\n", p);
-  return PyUnicode_FromString(p);
-  return obj;
+  Py_DECREF(div);
+  Py_DECREF(base36);
+  return Py_BuildValue("s", p);
 }
 
 static PyObject* py_myfn3(PyObject* self, PyObject* args)
@@ -324,31 +378,34 @@ static PyObject* py_myfn3(PyObject* self, PyObject* args)
   unsigned long long low = PyLong_AsUnsignedLongLongMask(obj);
   PyObject *shifted = PyNumber_Rshift(obj, PyLong_FromLong(64));
   unsigned long long high = PyLong_AsUnsignedLongLongMask(shifted);
+  Py_DECREF(shifted);
 
   // upper bound for the buffer will be 30 chars (for now)
-  int sz = 30;
-  PyStringObject *str;
-  str = (PyStringObject *) PyString_FromStringAndSize((char *)0, sz);
-  if (str == NULL)
-    return NULL;
+  const int sz = 30;
+  char buffer[sz];
+  //  PyUnicodeObject *str;
+  //  str = (PyUnicodeObject *) PyUnicode_FromStringAndSize((char *)0, sz);
+  //  if (str == NULL)
+  //    return NULL;
 
-  char *p = PyString_AS_STRING(str) + sz;
+  //  char *p = PyUnicode_AS_STRING(str) + sz;
+  char *p = buffer + sz - 1;
   *p = '\0';
   p -= 1;
 
   trike_div(high, low, &p);
 
-  if (p != PyString_AS_STRING(str)) {
-    char *q = PyString_AS_STRING(str);
+  if (p != buffer) {
+    char *q = buffer;
     assert(p > q);
     do {
     } while ((*q++ = *p++) != '\0');
     q--;
-    _PyString_Resize((PyObject **)&str,
-		     (Py_ssize_t) (q - PyString_AS_STRING(str)));
+    //    _PyUnicode_Resize((PyObject **)&str,
+    //		     (Py_ssize_t) (q - PyUnicode_AS_STRING(str)));
     //printf("yo2 %s\n", PyString_AS_STRING(str));
   }
-  return (PyObject *)str;
+  return Py_BuildValue("s", buffer);
 }
 
 static PyObject* py_myfn4(PyObject* self, PyObject* args)
@@ -358,31 +415,38 @@ static PyObject* py_myfn4(PyObject* self, PyObject* args)
   unsigned long long low = PyLong_AsUnsignedLongLongMask(obj);
   PyObject *shifted = PyNumber_Rshift(obj, PyLong_FromLong(64));
   unsigned long long high = PyLong_AsUnsignedLongLongMask(shifted);
+  Py_DECREF(shifted);
 
   // upper bound for the buffer will be 30 chars (for now)
-  int sz = 30;
-  PyStringObject *str;
-  str = (PyStringObject *) PyString_FromStringAndSize((char *)0, sz);
-  if (str == NULL)
-    return NULL;
+  const int sz = 30;
+  char buffer[sz];
+  //  PyUnicodeObject *str;
+  //  str = (PyUnicodeObject *) PyUnicode_FromStringAndSize((char *)0, sz);
+  //  if (str == NULL)
+  //    return NULL;
 
-  char *p = PyString_AS_STRING(str) + sz;
+  //  char *p = PyUnicode_AS_STRING(str) + sz;
+  char *p = buffer + sz - 1;
   *p = '\0';
-  
+  p -= 1;
+
   trike_div2(high, low, &p);
 
-  if (p != PyString_AS_STRING(str)) {
-    char *q = PyString_AS_STRING(str);
+  if (p != buffer) {
+    char *q = buffer;
     assert(p > q);
     do {
     } while ((*q++ = *p++) != '\0');
     q--;
-    _PyString_Resize((PyObject **)&str,
-		     (Py_ssize_t) (q - PyString_AS_STRING(str)));
+    //    _PyUnicode_Resize((PyObject **)&str,
+    //		     (Py_ssize_t) (q - PyUnicode_AS_STRING(str)));
+    //printf("yo2 %s\n", PyString_AS_STRING(str));
   }
-  return (PyObject *)str;
+  return Py_BuildValue("s", buffer);
 }
 
+#if PY_MAJOR_VERSION >= 3
+#else
 /*
  * Another function to be called from Python
  */
@@ -403,35 +467,24 @@ static PyObject* py_myWowFunction(PyObject* self, PyObject* args)
 
   //  printf("5\n");
   //  printf("5 %d\n", PyUnicode_Check(np));
-  //  printf("5 %d\n", PyBytes_Check(np));
-  char const *string_without_leaderC = PyBytes_AsString(np);
+  //  printf("5 %d\n", PyUnicode_Check(np));
+  char const *string_without_leaderC = PyString_AsString(np);
   //  printf("5x %p\n", string_without_leaderC);
-  PyObject *np2a = PyBytes_FromString(&string_without_leaderC[3]);
+  PyObject *np2a = PyString_FromString(&string_without_leaderC[3]);
   return np2a;
-  // Remove the leader
-  PyObject *np_ascii;
-  np_ascii = PyUnicode_AsASCIIString(np);
-  printf("7\n");
-  char const *string_without_leaderA = PyBytes_AsString(np_ascii);
-  printf("9x %p\n", string_without_leaderA);
-  char const *string_without_leader = &PyBytes_AS_STRING(np_ascii)[3];
-  printf("9y %p\n", string_without_leader);
-  PyObject *np2;
-  np2 = PyUnicode_FromString(&string_without_leader[3]);
-  printf("B\n");
-  return np2;
 }
-
+#endif
 
 /*
  * Bind Python function names to our C functions
  */
 static PyMethodDef mymodule_methods[] = {
   {"myFunction", py_myFunction, METH_VARARGS},
+  {"myFunction2", py_myFunction2, METH_VARARGS},
   {"myOtherFunction", py_myOtherFunction, METH_VARARGS},
-  {"myWowFunction", py_myWowFunction, METH_VARARGS},
 #if PY_MAJOR_VERSION >= 3
 #else
+  {"myWowFunction", py_myWowFunction, METH_VARARGS},
   {"myfn0", py_myfn0, METH_VARARGS},
 #endif
   {"myfn", py_myfn, METH_VARARGS},
--- profiling_timeit.py
+++ profiling_timeit.py
@@ -2,6 +2,13 @@ from __future__ import print_function
 import timeit
 import sys
 
+number = 10000
+repeat = 10
+if len(sys.argv) > 1:
+    number = int(sys.argv[1])
+if len(sys.argv) > 2:
+    repeat = int(sys.argv[2])
+print('number={}, repeat={}'.format(number, repeat))
 
 fn_setup = (
 #"""
@@ -15,6 +22,9 @@ from mymodule import myfn as encode_it
 #""",
 """
 from mymodule import myfn3 as encode_it
+""",
+"""
+from mymodule import myfn4 as encode_it
 """,
 """
 import sys
@@ -31,7 +41,7 @@ int_letter = '' if sys.version_info[0] == 3 else 'L'
 case_setup = (
 """
 the_int = 15651564330810771165{}
-the_enc = "3awvki7bh5eql"
+the_enc = '3awvki7bh5eql'
 """.format(int_letter)
 ,
 """
@@ -41,24 +51,16 @@ the_enc = 'cbn9ihut2vknewnwgwkylntma'
 )
 fixture = """assert encode_it(the_int) == the_enc, '"{}" != "{}"'.format(the_enc, encode_it(the_int))"""
 
-number = 10000
-repeat = 10
 idx = 0
 for case in case_setup:
     idx += 1
     print('='*80)
     print(case)
-#    if idx == 1:
-#        print('...skipping {}'.format(idx))
-#        continue
     for fn in fn_setup:
         idx += 1
         print('-'*40)
         print(fn)
         print('.'*20)
         print(fixture)
-#        if idx < 5:
-#            print('...skipping {}'.format(idx))
-#            continue
         the_time = timeit.repeat(fixture, case + fn, repeat=repeat)
         print('min / max / avg = {} {} {}'.format(min(the_time), max(the_time), sum(the_time) / repeat))
